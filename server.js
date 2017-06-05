e76c05c21f5ef62efa63f2156b70836f7cbafe7f
MaxaAboutAll <maxaaboutall@gmail.com>
2017-06-05 12:33:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/notes', (req, res) => {
