7d87e8dd229f926602e8b1b9c2142d95f6f7640f
MaxaAboutAll <maxaaboutall@gmail.com>
2017-06-05 12:35:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/notes', (req, res) => {
-    res.send('Her tebe v ochko');
-});
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
     require('./routes')(app, database);
