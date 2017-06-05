4a63c0a0d3e7f521e08d0f44d146d7cad02add90
MaxaAboutAll <maxaaboutall@gmail.com>
2017-06-05 12:13:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
-const port = 1337;
+const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
